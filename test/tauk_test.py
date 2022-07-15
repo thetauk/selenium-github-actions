d23e2054de4a827812a5234cced7c478f2bb37eb
Ranvir Chhina <ranvirchhina25@gmail.com>
2022-07-14 17:39:49 -0700

    tauk_test: adding options

--- test/tauk_test.py
+++ test/tauk_test.py
@@ -2,6 +2,7 @@ import unittest
 
 from dotenv import load_dotenv
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from tauk.tauk_webdriver import Tauk
 from webdriver_manager.chrome import ChromeDriverManager
@@ -10,11 +11,20 @@ load_dotenv(".env")
 Tauk()
 
 
+def chrome_options():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    return options
+
+
 class TaukTest(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome(
-            service=Service(ChromeDriverManager().install())
+            service=Service(ChromeDriverManager().install()),
+            chrome_options=chrome_options()
         )
         Tauk.register_driver(self.driver, unittestcase=self)
         self.driver.get("https://www.tauk.com/welcome")
